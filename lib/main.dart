0b3d63527fc4dcc8d7974a61d4df66b765da1783
indmind <sattanationmail@gmail.com>
2021-07-09 17:53:17 +0700

    Show available orders in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:diiket_models/all.dart';
-import 'package:driver/data/providers/auth/auth_provider.dart';
 import 'package:driver/ui/common/theme.dart';
 import 'package:driver/ui/pages/home_page.dart';
 import 'package:driver/ui/pages/auth/login_page.dart';
@@ -7,7 +5,6 @@ import 'package:driver/ui/pages/order_page.dart';
 import 'package:driver/ui/widgets/auth_wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
@@ -18,7 +15,7 @@ void main() async {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends HookWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,16 +1,20 @@
 import 'package:driver/data/providers/auth/auth_provider.dart';
+import 'package:driver/data/providers/order/available_orders_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'order_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   static String route = 'driver/home';
 
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final orderList = useProvider(availableOrdersProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Diiket Driver'),
@@ -37,6 +41,19 @@ class HomePage extends StatelessWidget {
                 Navigator.of(context).pushNamed(OrderPage.route);
               },
               child: Text('Ke Order Page'),
+            ),
+            SizedBox(height: 10),
+            Text('Order yang dapat diambil: '),
+            SizedBox(height: 4),
+            orderList.when(
+              data: (orders) => Text(orders
+                  .map(
+                    (o) =>
+                        '${o.id}: ${o.order_items?.map((e) => e.product?.name).join(', ')}',
+                  )
+                  .join('\n')),
+              loading: () => Text('loading'),
+              error: (e, s) => Text('error: $e'),
             ),
           ],
         ),
